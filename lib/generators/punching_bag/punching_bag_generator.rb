82997c040f9b9bffde8679026d02f37f4471fc6b
Fernando Segura Gomez <fernandoseguragomez@Fernandos-MacBook-Air.local>
2024-06-10 22:48:05 -0600

    fix issue with version

--- lib/generators/punching_bag/punching_bag_generator.rb
+++ lib/generators/punching_bag/punching_bag_generator.rb
@@ -7,7 +7,7 @@ class PunchingBagGenerator < Rails::Generators::Base
 
   def self.next_migration_number(dirname)
     sleep 1
-    if ActiveRecord::Base.timestamped_migrations
+    if timestamped_migrations?
       Time.now.utc.strftime("%Y%m%d%H%M%S")
     else
       "%.3d" % (current_migration_number(dirname) + 1)
@@ -16,5 +16,13 @@ class PunchingBagGenerator < Rails::Generators::Base
 
   def create_migration_file
     migration_template 'create_punches_table.rb', 'db/migrate/create_punches_table.rb'
+  end
+
+  def self.timestamped_migrations?
+    if Rails::VERSION::MAJOR >= 7
+      ActiveRecord.timestamped_migrations
+    else
+      ActiveRecord::Base.timestamped_migrations
+    end
   end
 end
